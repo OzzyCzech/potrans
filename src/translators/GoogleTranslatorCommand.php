210ad86467a61f264494ac350d6239aa9b6c608f
Roman Ožana <roman@ozana.cz>
2022-05-10 13:57:25 +0200

    Add cache

--- src/translators/GoogleTranslatorCommand.php
+++ src/translators/GoogleTranslatorCommand.php
@@ -7,10 +7,13 @@ use Gettext\Generator\PoGenerator;
 use Gettext\Loader\PoLoader;
 use Gettext\Translation;
 use Gettext\Translations;
+use Google\ApiCore\ValidationException;
 use Google\Cloud\Translate\V3\TranslationServiceClient;
 use potrans\translators\DeepLTranslator;
 use potrans\translators\GoogleTranslator;
 use potrans\translators\ITranslator;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\NullAdapter;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Exception\RuntimeException;
@@ -20,6 +23,8 @@ use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class GoogleTranslatorCommand extends Command {
 
@@ -32,7 +37,8 @@ class GoogleTranslatorCommand extends Command {
 			->addOption('wait', null, InputOption::VALUE_REQUIRED, 'Wait between translations in milliseconds', false)
 			->addOption('credentials', null, InputOption::VALUE_REQUIRED, 'Path to Google Credentials file', './credentials.json')
 			->addOption('project', null, InputOption::VALUE_REQUIRED, 'Google Cloud Project ID')
-			->addOption('location', null, InputOption::VALUE_REQUIRED, 'Google Cloud Location', 'global');
+			->addOption('location', null, InputOption::VALUE_REQUIRED, 'Google Cloud Location', 'global')
+			->addOption('cache', null, InputOption::VALUE_NEGATABLE, 'Load from cache or not', true);
 	}
 
 	/**
@@ -52,111 +58,131 @@ class GoogleTranslatorCommand extends Command {
 	 * @throws \Google\ApiCore\ValidationException
 	 * @throws \Google\ApiCore\ApiException
 	 */
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		$from = $input->getOption('from');
-		$to = $input->getOption('to');
-		$inputFile = $input->getArgument('input');
-		$location = $input->getOption('location');
-		$wait = $input->getOption('wait');
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$cache = new FilesystemAdapter('google', 3600, 'cache');
 
-		// load translations
-		$translations = $this->loadPoFile($inputFile);
+		try {
 
-		// credentials
-		$credentials = $input->getOption('credentials');
-		if (!is_file($credentials)) {
-			throw new InvalidOptionException('Missing credentials or API key');
-		}
+			$from = $input->getOption('from');
+			$to = $input->getOption('to');
+			$inputFile = $input->getArgument('input');
+			$location = $input->getOption('location');
+			$wait = $input->getOption('wait');
 
-		// project
-		$project = $input->getOption('project');
-		if (!$project) {
-			throw new InvalidOptionException('Project ID is required');
-		}
+			// load translations
+			$translations = $this->loadPoFile($inputFile);
 
-		// output directory
-		$outputDir = realpath($input->getArgument('output')) . DIRECTORY_SEPARATOR;
-		if (!is_dir($outputDir)) {
-			throw new InvalidOptionException('Invalid directory path: ' . $outputDir);
-		}
+			// credentials
+			$credentials = $input->getOption('credentials');
+			if (!is_file($credentials)) {
+				throw new InvalidOptionException('Missing credentials or API key');
+			}
+			$translator = new TranslationServiceClient(['credentials' => $credentials]);
 
-		// translator
-		$output->writeln(
-			[
-				'------------------------------',
-				' PO trans translator',
-				'------------------------------',
-				'Input: ' . $inputFile,
-				'Translate: from ' . $from . ' to ' . $to,
-				'Output dir: ' . $outputDir,
-				'Translating:',
-			]
-		);
+			// project
+			$project = $input->getOption('project');
+			if (!$project) {
+				throw new InvalidOptionException('Project ID is required');
+			}
 
-		$progress = new ProgressBar($output, count($translations));
-		$translator = new TranslationServiceClient(['credentials' => $credentials]);
+			// output directory
+			$outputDir = realpath($input->getArgument('output')) . DIRECTORY_SEPARATOR;
+			if (!is_dir($outputDir)) {
+				throw new InvalidOptionException('Invalid directory path: ' . $outputDir);
+			}
 
-		$translated = 0; // counter
-		/** @var Translation $sentence */
-		foreach ($translations as $sentence) {
+			// translator
+			$output->writeln(
+				[
+					'------------------------------',
+					' PO trans translator',
+					'------------------------------',
+					'<comment>Input</comment>: ' . $inputFile,
+					'<comment>Translate</comment>: from ' . $from . ' to ' . $to,
+					'<comment>Output dir</comment>: ' . $outputDir,
+					'<comment>Translating progress</comment>:',
+				]
+			);
 
-			if (!$sentence->getTranslation() || $input->getOption('all')) {
-				// translated counter
-				$translated++;
+			$progress = new ProgressBar($output, count($translations));
 
-				$response = $translator->translateText(
-					[$sentence->getOriginal()],
-					$to,
-					TranslationServiceClient::locationName($project, $location)
-				);
+			$translated = 0; // counter
+			/** @var Translation $sentence */
+			foreach ($translations as $sentence) {
 
-				$sentence->translate($response->getTranslations()[0]->getTranslatedText());
+				if (!$sentence->getTranslation() || $input->getOption('all')) {
+					// translated counter
+					$translated++;
 
-				// verbose mode show everything
-				if ($output->isVeryVerbose()) {
-					$output->writeln(
-						[
-							'-------------------------------------------------------------------------',
-							' > ' . $sentence->getOriginal(),
-							' > ' . $sentence->getTranslation(),
-						]
-					);
+					$translation = $cache->getItem(md5($sentence->getOriginal()));
+
+					if (!$translation->isHit() || !$input->getOption('cache')) {
+
+						$response = $translator->translateText(
+							[$sentence->getOriginal()],
+							$to,
+							TranslationServiceClient::locationName($project, $location),
+							['sourceLanguageCode' => $from]
+						);
+
+						$translation->set($response->getTranslations()[0]->getTranslatedText());
+
+						// save to cache
+						if ($input->getOption('cache')) {
+							$cache->save($translation);
+						}
+					}
+
+					$sentence->translate($translation->get());
+
+					// verbose mode show everything
+					if ($output->isVeryVerbose()) {
+						$output->writeln(
+							[
+								'-------------------------------------------------------------------------',
+								' > <info>' . $sentence->getOriginal() . '</info>',
+								' > <comment>' . $sentence->getTranslation() . '</comment>',
+							]
+						);
+					}
 				}
+
+				// progress
+				if (!$output->isVeryVerbose()) {
+					$progress->advance();
+				}
+
+				if ($wait) usleep($wait);
 			}
 
-			// progress
 			if (!$output->isVeryVerbose()) {
-				$progress->advance();
+				$progress->finish();
+				$output->writeln('');
 			}
 
-			if ($wait) usleep($wait);
+			$output->writeln('<comment>Translated :</comment> ' . $translated . ' sentences');
+
+			// MO file output
+			$moGenerator = new MoGenerator();
+			$moOutputFile = $outputDir . pathinfo($inputFile, PATHINFO_FILENAME) . '.mo';
+			if ($output->isVeryVerbose()) {
+				$output->writeln('<comment>Writing new MO File</comment>: ' . $moOutputFile);
+			}
+			$moGenerator->generateFile($translations, $moOutputFile);
+
+			// PO file output
+			$poGenerator = new PoGenerator();
+			$poOutputFile = $outputDir . pathinfo($inputFile, PATHINFO_FILENAME) . '.po';
+			if ($output->isVeryVerbose()) {
+				$output->writeln('<comment>Writing new PO File</comment>: ' . $poOutputFile);
+			}
+			$poGenerator->generateFile($translations, $poOutputFile);
+
+			$output->writeln('<info>DONE!</info>');
+		} catch (\Throwable $error) {
+			$output->writeln('<error>ERROR: ' . $error->getMessage() . '</error>');
+			return Command::FAILURE;
 		}
-
-		if (!$output->isVeryVerbose()) {
-			$progress->finish();
-			$output->writeln('');
-		}
-
-		$output->writeln('Translated sentences count: ' . $translated);
-
-		// MO file output
-		$moGenerator = new MoGenerator();
-		$moOutputFile = $outputDir . pathinfo($inputFile, PATHINFO_FILENAME) . '.mo';
-		if ($output->isVeryVerbose()) {
-			$output->writeln('Writing new MO File: ' . $moOutputFile);
-		}
-		$moGenerator->generateFile($translations, $moOutputFile);
-
-		// PO file output
-		$poGenerator = new PoGenerator();
-		$poOutputFile = $outputDir . pathinfo($inputFile, PATHINFO_FILENAME) . '.po';
-		if ($output->isVeryVerbose()) {
-			$output->writeln('Writing new PO File: ' . $poOutputFile);
-		}
-		$poGenerator->generateFile($translations, $poOutputFile);
-
-		// done!
-		$output->writeln('DONE!');
 
 		return Command::SUCCESS;
 	}
