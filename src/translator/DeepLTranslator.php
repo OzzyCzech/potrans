bccd8303c6014568316f7eba560097d323cf3392
Aimeos <aimeos@aimeos.org>
2025-02-17 14:10:35 +0100

    Use POT file for mapping in DeepLTranslator

--- src/translator/DeepLTranslator.php
+++ src/translator/DeepLTranslator.php
@@ -7,18 +7,20 @@ use Gettext\Translation;
 class DeepLTranslator extends TranslatorAbstract {
 
 	private \DeepL\Translator $translator;
+	private ?\Gettext\Translations $pot;
 	private ?string $regex;
 
-	public function __construct(\DeepL\Translator $translator, string $regex = null) {
+	public function __construct(\DeepL\Translator $translator, \Gettext\Translations $pot = null, string $regex = null) {
 		$this->translator = $translator;
 		$this->regex = $regex;
+		$this->pot = $pot;
 	}
 
 	/**
 	 * @throws \DeepL\DeepLException
 	 */
 	public function getTranslation(Translation $sentence): string {
-		$text = $sentence->getOriginal();
+		$text = $this->pot?->find($sentence->getContext(), $sentence->getOriginal())?->getTranslation() ?: $sentence->getOriginal();
 		$response = $this->translator->translateText(
 			$this->regex ? preg_replace('/(' . $this->regex . ')/', '<keep>$1</keep>', $text) : $text,
 			$this->from,
